8cb0eaca6d568065a2761de4467e3fe7946fdbb6
Florian Enner <florian@enner.org>
2017-07-26 22:33:02 +0200

    overhauled basic kinematics example

--- basic/ex4_kinematics.m
+++ basic/ex4_kinematics.m
@@ -1,23 +1,30 @@
+%% HebiKinematics
 % This example shows howto use the HebiKinematics API to calculate forward
-% kinematics and Jacobians.
-% 
-% Requirements:  MATLAB 2013b or higher
+% kinematics, Jacobians, as well as torques to compensate for accelerations
+% due to gravity and the dynamics.
 %
-% Author:        Florian Enner
-% Created:       13 July, 2017
-% API:           hebi-matlab-1.0
+%%
+%
+% <html>
+% <table border=0>
+%   <tr><td>Created</td><td>July 13, 2017</td></tr>
+%   <tr><td>Last Update</td><td>July 26, 2017</td></tr>
+%   <tr><td>API Version</td><td>hebi-matlab-1.0</td></tr>
+%   <tr><td>Requirements</td><td>MATLAB 2013b or higher</td></tr>
+% </table>
+% </html>
 %
 % Copyright 2017 HEBI Robotics
 
 %% Define kinematic structure using HEBI components
 % The addBody method creates a serial chain of bodies that describe the 
-% kinematic relation of a robot. A 'body' can be a rigid link as well as a 
+% kinematic relation of a robot. A body can be a rigid link or a 
 % dynamic element. The first body represents the base and the last body 
 % represents the end-effector.
 %
 % The following code creates a representation of the rendered 5-DoF arm  
-% shown below. For more information on the available parts, please consult
-% the help files and/or the online documentation.
+% shown below. For more information on available bodies, please consult
+% the help files and/or the online documentation at <http://docs.hebi.us>.
 %
 % <<resources/5dof_fk.png>>
 %
@@ -37,15 +44,7 @@ kin.addBody('X5-1');
 % Display
 display(kin);
 
-%% Programmatically access body info
-bodyInfo = kin.getBodyInfo();
-display(bodyInfo);
-
-%% Programmatically access joint info
-jointInfo = kin.getJointInfo();
-display(jointInfo);
-
-%% Get Forward kinematics
+%% Calculate Forward Kinematics
 % While the HebiKinematics and the HebiGroup APIs were desigend to work
 % well together, the HebiKinematics API is independent and can be used
 % by itself. To keep these examples simple, we use pre-defined position
@@ -64,7 +63,7 @@ endEffector = kin.getForwardKinematicsEndEffector(position);
 display(endEffector);
 
 
-%% Get Jacobian
+%% Calculate Jacobian
 % The calls to get the Jacobian work the same way as the forward
 % kinematics.
 
@@ -74,3 +73,79 @@ J = kin.getJacobian('output', position);
 % 6 x numDoF matrix
 J_endEffector = kin.getJacobianEndEffector(position);
 display(J_endEffector);
+
+%% Gravity Compensation
+% There are many use cases such as teach-repeat that benefit from an arm
+% being in a compliant 'zero-gravity' mode. The Kinematics API provides
+% convenience methods that help with calculating compensatory torques.
+
+% Calculate compensatory torques at position zero w/ gravity pointing in
+% the negative z direction
+gravityVec = [0 0 -1];
+position = zeros(1, kin.getNumDoF);
+gravCompTorques = kin.getGravCompTorques(position, gravityVec);
+
+%%%
+% The following example continuously compensates for gravity on a 2 dof
+% arm. It works with any serial chain robot configuration provided
+% that the actuators in the group match the kinematic configuration.
+%
+% <<resources/2dof_RR_ik_xy.png>>
+%
+
+% Setup 2 dof planar RR arm (could be any number of DoF)
+kin = HebiKinematics();
+kin.addBody('X5-4'); % base joint
+kin.addBody('X5-Link', 'ext', 0.35, 'twist', pi);
+kin.addBody('X5-1');
+kin.addBody('X5-Link', 'ext', 0.25, 'twist', pi);
+
+% Setup the group that corresponds to the cnfiguration
+group = HebiLookup.newGroupFromNames('2dof', {'base', 'knee'});
+
+% Determine the direction of gravity based on the built-in IMU 
+% (assumes fixed base)
+fbk = group.getNextFeedback();
+gravityVec = -[fbk.accelX(1) fbk.accelY(1) fbk.accelZ(1)];
+
+% Continuously command a 'weight-less' mode
+t0 = tic();
+cmd = CommandStruct();
+while toc(t0) < 5
+    fbk = group.getNextFeedback();
+    cmd.torque = kin.getGravCompTorques(fbk.position, gravityVec);
+    group.send(cmd);
+end
+
+%% Dynamics Compensation
+% Similarly to gravity compensation, HebiKinematics also provides a way to
+% calculate torques to compensate for the dynamics.
+%
+% It does require knowledge of positions, velocities, and accelerations at
+% a given point, so it is primarily useful when being combined with a
+% trajectory, such as provided by the HebiTrajectory API (later example).
+
+% Create sample sinusoidal motion
+time = 0; 
+freq = 1 * (2*pi);  % 1 Hz 
+amp = 1; 
+allDoF = ones(1,group.getNumModules);
+
+% Sample point in trajectory
+cmdPosition = amp * sin( freq * time ) * allDoF;
+cmdVelocity = freq * amp * cos( freq * time ) * allDoF;
+cmdAcceleration = -freq^2 * amp * sin( freq * time ) * allDoF;
+
+% Calculate torques at sampled point within trajectory
+torques = kin.getDynamicCompTorques(...
+    fbk.position, ...
+    cmdPosition, ...
+    cmdVelocity, ...
+    cmdAcceleration);
+
+% Display
+display(cmdPosition);
+display(cmdVelocity);
+display(cmdAcceleration);
+display(torques);
+
