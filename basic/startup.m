4252d3f01540f6c269e21f7c0e8be2a58c15c490
Florian Enner <florian@enner.org>
2018-03-09 17:47:38 +0100

    changed docs so users aren't expected to always download the latest API themselves

---
+++ basic/startup.m
@@ -0,0 +1,8 @@
+function [] = startup()
+% startup sets up libraries and should be run once on startup.
+
+localDir = fileparts(mfilename('fullpath'));
+includeScript = fullfile(localDir, '..', 'include', 'include.m');
+run(includeScript);
+
+end
