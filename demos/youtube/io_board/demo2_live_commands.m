e243818405fc59d6a15afd2d44137e54b9d9be34
iamtesch <iamtesch@gmail.com>
2018-05-21 17:25:54 -0400

    Renaming pins - demo 2

--- demos/youtube/io_board/demo2_live_commands.m
+++ demos/youtube/io_board/demo2_live_commands.m
@@ -1,10 +1,10 @@
 %% I/O Board Pin Mapping
-potentiometer = 'b1'; % analog input (0-5 volts)
-loadCell      = 'b8'; % analog input (0-5 volts)
-inputButton   = 'e1'; % digital input (0 or 1)
-led_r         = 'c6'; % digital output (0 or 1)
-led_g         = 'c7'; % digital output (0 or 1)
-led_b         = 'c8'; % digital output (0 or 1)
+potentiometer = 'a1'; % analog input (0-5 volts)
+loadCell      = 'a8'; % analog input (0-5 volts)
+inputButton   = 'b1'; % digital input (0 or 1)
+led_r         = 'e6'; % digital output (0 or 1)
+led_g         = 'e7'; % digital output (0 or 1)
+led_b         = 'e8'; % digital output (0 or 1)
 
 %% Setup Groups
 ioBoard = HebiLookup.newGroupFromNames('*', 'IO_basic');
