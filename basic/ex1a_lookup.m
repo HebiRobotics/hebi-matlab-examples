b013f099b9fb98c79dfd3fd189c3c5b790c3c7e2
Dave Rollinson <daverolli@gmail.com>
2018-06-25 15:53:00 -0400

    Added Lookup examples.

---
+++ basic/ex1a_lookup.m
@@ -0,0 +1,17 @@
+% Initialize the lookup of modules and display information for any modules
+% on the network.
+%
+% Assumes that you have run 'startup.m' in this folder.
+%
+% HEBI Robotics
+% Jun 2018
+
+HebiLookup.initialize();
+
+disp(HebiLookup);
+
+disp(' NOTE:');
+disp('  The table above should show the information for all the modules');
+disp('  on the local network.  If this table is empty make sure that the');
+disp('  modules are connected, powered on, and that the status LEDs are'); 
+disp('  displaying a green soft-fade.');
---
+++ basic/ex1b_newGroup_single_module.m
@@ -0,0 +1,17 @@
+% Initialize the lookup of modules and display information for any modules
+% on the network.
+%
+% Assumes that you have run 'startup.m' in this folder.
+%
+% HEBI Robotics
+% Jun 2018
+
+HebiLookup.initialize(); % Only needed once per session, but it doesn't
+                         % hurt to do this every time we run a new script,
+                         % just in case something changed on the network.
+
+familyName = 'My Family';
+moduleNames = 'My First Module';
+
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
