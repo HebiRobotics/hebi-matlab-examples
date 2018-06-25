79167a7679a596ee7a1005b594890ac6547a49af
Dave Rollinson <daverolli@gmail.com>
2018-06-25 15:16:35 -0400

    Removed old files.  Kept startup.m

--- basic/startup.m
+++ basic/startup.m
@@ -1,5 +1,8 @@
 function [] = startup()
-% startup sets up libraries and should be run once on startup.
+% startup.m sets up libraries and paths and should be run once on startup.
+% 
+% HEBI Robotics
+% Jun 2018
 
 localDir = fileparts(mfilename('fullpath'));
 includeScript = fullfile(localDir, '..', 'include', 'include.m');
