c531fd25f6f53c939d50d7e91034d34f8567e3a3
Dave Rollinson <daverolli@gmail.com>
2018-07-05 13:48:15 -0400

    Added lookup examples to io_board and mobile_io
    I made the comments a little more generic and duplicated the code in all 3 sub-directories for now.  I think this is a little more clear than trying to have some examples that are 'common' and some examples that are hardware-specific.

---
+++ basic/io_board/ex2a_feedback.m
@@ -0,0 +1,50 @@
+% Get feedback from a module and plot it live.
+%
+% Assumes that you have a group created with at least 1 module in it.
+%
+% HEBI Robotics
+% June 2018
+
+clear *;
+close all;
+
+HebiLookup.initialize();
+
+% Use Scope to change select a module and change the name and family to
+% match the names below.  Following examples will use the same names.
+familyName = 'My Family';
+moduleNames = 'Test I/O Board'; 
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+exampleDuration = 10; % sec
+exampleTimer = tic;
+
+disp('  Plotting gyro data from the module IMU.');
+disp('  Move the module around to make the feedback interesting...');  
+
+% Flag so that we only set the title, etc of plot one time
+isFirstDraw = true;
+
+figure(1);
+clf;
+
+while toc(exampleTimer) < exampleDuration
+    
+   fbk = group.getNextFeedback();
+   
+   bar( [fbk.gyroX fbk.gyroY fbk.gyroZ] );
+   
+   yAxisMaxLim = 15; 
+   yAxisMinLim = -15;
+   ylim([yAxisMinLim yAxisMaxLim]);
+   
+   title( 'Module Gyro Feedback' );
+   xlabel( 'Axis' );
+   ylabel( 'Angular Velocity (rad/sec)');
+   grid on;
+
+   drawnow;
+
+end
+
+disp('  All Done!');
---
+++ basic/io_board/ex2b_feedback_w_logging.m
@@ -0,0 +1,69 @@
+% Get feedback from a module, log in the background, and plot offline.
+%
+% Assumes that you have a group created with at least 1 module in it.
+%
+% HEBI Robotics
+% June 2018
+
+clear *;
+close all;
+
+HebiLookup.initialize();
+
+% Use Scope to change select a module and change the name and family to
+% match the names below.  Following examples will use the same names.
+familyName = 'My Family';
+moduleNames = 'Test I/O Board';  
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+exampleDuration = 10; % sec
+exampleTimer = tic;
+
+group.startLog( 'dir', 'logs' );  % Starts logging in the background
+
+disp('  Use Scope to command the module and make it move...');
+
+fbk = group.getNextFeedback();
+
+figure(1);
+clf;
+
+while toc(exampleTimer) < exampleDuration
+    
+   fbk = group.getNextFeedback();
+   
+   bar( fbk.velocity );
+   
+   yAxisMaxLim = 5; 
+   yAxisMinLim = -5;
+   ylim([yAxisMinLim yAxisMaxLim]);
+   
+   title( 'Module Output Velocity' );
+   ylabel( 'Angular Velocity (rad/sec)');
+   grid on;
+   
+   drawnow;
+   
+end
+
+disp('  All done!');
+
+log = group.stopLog();  % Stops background logging
+
+% Plot the logged position feedback
+figure(101);
+plot(log.time,log.position);
+title('Position');
+xlabel('time (sec)');
+ylabel('position (rad)');
+grid on;
+
+% Plot the logged velocity feedback
+figure(102);
+plot(log.time,log.velocity);
+title('Velocity');
+xlabel('time (sec)');
+ylabel('velocity (rad/sec)');
+grid on;
+
+
