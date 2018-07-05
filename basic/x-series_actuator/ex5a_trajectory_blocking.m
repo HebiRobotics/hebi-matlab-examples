ad9df3706b5a17564c290f14a9a65c5c898f7c1d
Dave Rollinson <daverolli@gmail.com>
2018-07-05 18:05:40 -0400

    Added trajectory examples.

---
+++ basic/x-series_actuator/ex5a_trajectory_blocking.m
@@ -0,0 +1,54 @@
+% Generate a trajectory and execute it using the blocking API.  This code
+% does the exact same commands as the next non-blocking API example.
+%
+% Assumes that you have a group created with 1 module in it.
+%
+% HEBI Robotics
+% June 2018
+
+clear *;
+close all;
+
+HebiLookup.initialize();
+
+familyName = 'My Family';
+moduleNames = 'Test Module';  
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+cmd = CommandStruct();
+
+exampleDuration = 10; % sec
+exampleTimer = tic;
+
+group.startLog( 'dir', 'logs' );
+
+trajGen = HebiTrajectoryGenerator();
+
+% Go from 0 to 180-degrees in 3 seconds
+waypoints = [ 0; 
+              pi ];
+time = [ 0 3 ];
+
+trajectory = trajGen.newJointMove( waypoints, 'time', time );
+
+% Visualize the trajectory
+HebiUtils.plotTrajectory(trajectory);
+
+% This function executes the trajectory using the 'blocking' API.  This
+% means that your program will go into this function and 'block' the rest
+% of the code from running until it is done.  This makes the high-level
+% code convenient, but it makes it more difficult to grab and analyze
+% feedback while the motion is being executed.
+trajGen.executeTrajectory( group, trajectory );
+
+% Reverse the waypoints and to go back to the first position
+waypoints = flipud(waypoints);
+trajectory = trajGen.newJointMove( waypoints, 'time', time );
+trajGen.executeTrajectory( group, trajectory );
+
+log = group.stopLog(); 
+
+% Plot using some handy helper functions
+HebiUtils.plotLogs( log, 'position', 'figNum', 101 );
+HebiUtils.plotLogs( log, 'velocity', 'figNum', 102 );
+
---
+++ basic/x-series_actuator/ex5b_trajectory_non_blocking.m
@@ -0,0 +1,86 @@
+% Generate a trajectory and execute it using the non-blocking API.  This
+% code does the exact same commands as the previous blocking API example.
+%
+% Assumes that you have a group created with 1 module in it.
+%
+% HEBI Robotics
+% June 2018
+
+clear *;
+close all;
+
+HebiLookup.initialize();
+
+familyName = 'My Family';
+moduleNames = 'Test Module';  
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+cmd = CommandStruct();
+
+exampleDuration = 10; % sec
+exampleTimer = tic;
+
+group.startLog( 'dir', 'logs' );
+
+trajGen = HebiTrajectoryGenerator();
+
+% Go from 0 to 180-degrees in 3 seconds
+waypoints = [ 0; 
+              pi ];
+time = [ 0 3 ];
+
+trajectory = trajGen.newJointMove( waypoints, 'time', time );
+
+% Visualize the trajectory
+HebiUtils.plotTrajectory(trajectory);
+
+% This function executes the trajectory using the 'non-blocking' API.  In
+% this format, we have to make a while loop to step through the trajectory
+% at each timestep, evaluate the trajectory to get commands, and send them
+% to the actuator.  This makes the top-level code more complex, but it has
+% the advantage that it is easy to evaluate feedback every timestep and
+% change the trajectory or some other behavior
+t0 = tic();
+t = toc(t0);
+while t < trajectory.getDuration()
+    
+    fbk = group.getNextFeedback();
+
+    % Get trajectory state at the current time
+    t = toc(t0);
+    [pos, vel, accel] = trajectory.getState(t);
+
+    % Send the commands
+    cmd.position = pos;
+    cmd.velocity = vel;
+    group.send(cmd);
+
+end
+
+% Reverse the waypoints and to go back to the first position
+waypoints = flipud(waypoints);
+trajectory = trajGen.newJointMove( waypoints, 'time', time );
+
+t0 = tic();
+t = toc(t0);
+while t < trajectory.getDuration()
+    
+    fbk = group.getNextFeedback();
+
+    % Get trajectory state at the current time
+    t = toc(t0);
+    [pos, vel, accel] = trajectory.getState(t);
+
+    % Send the commands
+    cmd.position = pos;
+    cmd.velocity = vel;
+    group.send(cmd);
+
+end
+
+log = group.stopLog(); 
+
+% Plot using some handy helper functions
+HebiUtils.plotLogs( log, 'position', 'figNum', 101 );
+HebiUtils.plotLogs( log, 'velocity', 'figNum', 102 );
+
