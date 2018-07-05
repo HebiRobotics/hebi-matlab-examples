eb8ac61a91b29a14e2161c664f860b3d415937f7
Dave Rollinson <daverolli@gmail.com>
2018-07-05 16:09:24 -0400

    Added example gains file and loading / saving examples

---
+++ basic/x-series_actuator/ex4c_gains_saving_xml.m
@@ -0,0 +1,20 @@
+% Save and load gains using the cross-API gains XML format.
+%
+% Assumes that you have a group created with 1 module in it.
+%
+% HEBI Robotics
+% July 2018
+
+clear *;
+close all;
+
+HebiLookup.initialize();
+
+familyName = 'My Family';
+moduleNames = 'Test Module';  
+group = HebiLookup.newGroupFromNames( familyName, moduleNames );
+
+% Get the gains that are currently active on the module
+gains = group.getGains();
+
+HebiUtils.saveGains(gains,'/gains/myGains.XML');
