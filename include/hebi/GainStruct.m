308d22f27f705877472d4c074e865d7260dda21a
Dave Rollinson <daverolli@gmail.com>
2018-07-10 09:03:07 -0400

    Updated docs for GainStruct

--- include/hebi/GainStruct.m
+++ include/hebi/GainStruct.m
@@ -2,20 +2,33 @@ function struct = GainStruct()
     % GainStruct can be used to set gains of groups
     %
     %   The struct created by this function can be used to set a variety of
-    %   gains on a group of modules. Please consult the online
-    %   documentation to find out more about the individual gain settings.
+    %   gains and control parameters on a group of modules.
     %
-    %   NaNs for any values in gains or control parmaters are ignored. In
-    %   these cases any existing setting on the module for that parameter
-    %   will remain unmodified.
+    %   GainStructs can be loaded and saved to an xml file format with 
+    %   functions provided in HebiUtils.
     %
-    %   Example:
-    %       % Set the control strategy of a 2 module group to strategy 4
+    %   Empty entries in the struct and NaNs for any individual values in 
+    %   gains or control parmaters are ignored. In these cases any existing 
+    %   setting on the module for that parameter will remain unmodified.  
+    %
+    %   The online documentation provides more information about the 
+    %   individual gain settings and control parameters:
+    %   http://docs.hebi.us/core_concepts.html#controller_gains
+    %
+    %   Example (loading gains):
+    %       gains = HebiUtils.loadGains('myGains.xml');
+    %       group.send('gains');
+    %
+    %   Example (saving gains)
+    %       gains = group.getGains();
+    %       HebiUtils.saveGains(gains, 'myGains.xml');
+    %
+    %   Example (manually setting gains):
     %       gains = GainStruct()
     %       gains.controlStrategy = [4 4];
     %       group.send('gains', gains);
     %
-    %   See also HebiLookup, HebiGroup
+    %   See also HebiGroup, HebiUtils
     
     %   Copyright 2014-2018 HEBI Robotics, Inc.
     struct = javaObject(hebi_load('GainStruct'));
