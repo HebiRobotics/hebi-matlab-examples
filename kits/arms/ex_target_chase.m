cf41df2672a146f373bed9f06c130dde95cc9c57
Dave Rollinson <daverolli@gmail.com>
2018-07-10 14:02:30 -0400

    Updated to use HebUtils.plotLogs()

--- kits/arms/ex_target_chase.m
+++ kits/arms/ex_target_chase.m
@@ -95,7 +95,9 @@ end
 hLog = group.stopLogFull();
 
 % Plot the commands and feedback
-plotLogCommands(hLog, group)
+HebiUtils.plotLogs(hebilog, 'position', 'figNum', 101);
+HebiUtils.plotLogs(hebilog, 'velocity', 'figNum', 102);
+HebiUtils.plotLogs(hebilog, 'effort', 'figNum', 103);
 
 %% Get target coordinates (from mouse, but could also be from e.g. video)
 function [xyz] = getTargetCoordinates()
