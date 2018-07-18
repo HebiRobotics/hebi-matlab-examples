f08c76700b0c2209e347cab34385f59ff8fece77
Dave Rollinson <daverolli@gmail.com>
2018-07-17 22:37:18 -0400

    Updated gains example comments

--- basic/x-series_actuator/ex4a_gains_positionKp.m
+++ basic/x-series_actuator/ex4a_gains_positionKp.m
@@ -20,11 +20,11 @@ familyName = 'My Family';
 moduleNames = 'Test Module';  
 group = HebiLookup.newGroupFromNames( familyName, moduleNames );
 
+% Like the commmand struct, the gains struct will have fields for every 
+% possible gain value.  Fields that are empty [] or NaN will be ignored 
+% when sending.
 cmd = CommandStruct();
-gains = GainStruct(); % Like the commmand struct, the gains struct will 
-                      % have fields for every possible gain value.  Fields 
-                      % that are empty [] or NaN will be ignored when
-                      % sending.
+gains = GainStruct(); 
 
 exampleDuration = 4;    % [sec]
 
@@ -39,32 +39,36 @@ newPositionKpGains = [ 0.2 0.5 10 ];
 
 for i = 1:length(newPositionKpGains)
     
+    % Update gains struct and send to the actuator
     gains.positionKp = newPositionKpGains(i);
-    
     group.send( 'gains', gains );
     disp(['  Setting Kp to: ' num2str(newPositionKpGains(i))]);
 
+    % Command a step function to show actuator response
     exampleTimer = tic;
     while toc(exampleTimer) < exampleDuration
-
+        
+        % Get feedback to limit the loop rate
         fbk = group.getNextFeedback();
-
+        
+        % Calculate position step function and send command
         if rem( toc(exampleTimer), 2*stepPeriod ) > stepPeriod
            cmd.position = stepPosition;
         else
            cmd.position = 0;
         end
-
         group.send(cmd);
 
     end
 end
 
+% Pause to let the actuator response to settle
 pause(stepPeriod);
 
 disp('  All done!');
 
-log = group.stopLog();  % Stops background logging
+% Stops background logging
+log = group.stopLog();  
 
 % Plot using some handy helper functions
 HebiUtils.plotLogs( log, 'position', 'figNum', 101 );
