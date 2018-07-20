e07d4562f2e2bdcdefaf2764f54b7decd1722627
Dave Rollinson <daverolli@gmail.com>
2018-07-20 11:54:23 -0400

    Made mobile base setup more consistent.

--- kits/rosie/setupDiffDriveBase.m
+++ kits/rosie/setupDiffDriveBase.m
@@ -1,16 +1,20 @@
 function [ params, trajGen ] = setupDiffDriveBase()
-%SETUPEDWARD creates various models for the Edward kit
+%SETUPDIFFDRIVEBASE initializes the kinematic and control parameters for a
+%2-wheeled diff-drive base.
+%
+% FRAME CONVENTION:
+% ORIGIN = MID-POINT BETWEEN THE WHEELS
+% +X-AXIS = FORWARD
+% +Y-AXIS = LEFT
+% +Z-AXIS = UP
 
-% Initialize structs
-params = struct(); % various constant parameters
+wheelDiameter = .200; % [m]
+wheelBase = .430; % [m]
 
-%% %%%%%%%%%%%%%%%%%%%%%
-% Omni Base Kinematics %
-%%%%%%%%%%%%%%%%%%%%%%%%
+wheelRadius = wheelDiameter / 2;  % [m]
 
-wheelRadius = .200 / 2;  % m
-wheelBase = .430;   % m (center chassis to each wheel)
-
+% Fill in a struct with all this information to return out into the main
+% demo script.
 params.wheelRadius = wheelRadius;
 params.wheelBase = wheelBase;
 params.maxLinSpeed = 0.8; % m/s
@@ -19,30 +23,32 @@ params.maxRotSpeed = params.maxLinSpeed / wheelBase; % rad/s
 params.chassisCoM = [0; 0; wheelRadius + 0.005];  % m
 params.chassisMass = 10;  % kg
 
-params.numWheels = 3;
+params.wheelNames = { '_LeftWheel', ...
+                      '_RightWheel' };
+params.numWheels = 2;
 
-wheelBaseFrames(:,:,1) = eye(4); % left
-wheelBaseFrames(:,:,2) = eye(4); % right
-wheelBaseFrames(1:3,4,1) = [0; wheelBase/2; 0];
-wheelBaseFrames(1:3,4,2) = [0; -wheelBase/2; 0];
+% Transform from the origin of the chassis to the Left Wheel
+wheelBaseFrames(:,:,1) = eye(4); 
+wheelBaseFrames(1:3,4,1) = [ 0; 
+                             wheelBase/2; 
+                             0 ];
 wheelBaseFrames(1:3,1:3,1) = R_z(pi/2) * R_y(pi/2);
+
+% Transform from the origin of the chassis to the Right Wheel
+wheelBaseFrames(:,:,2) = eye(4); % right
+wheelBaseFrames(1:3,4,2) = [ 0; 
+                            -wheelBase/2; 
+                             0 ];             
 wheelBaseFrames(1:3,1:3,2) = R_z(-pi/2) * R_y(pi/2);
 
 params.wheelBaseFrames = wheelBaseFrames;
 
-% Maps XYZ chassis velocities to wheel velocities
-%
-% FRAME CONVENTION (FROM THE "DRIVER'S SEAT"):
-% +X-AXIS = FORWARD
-% +Y-AXIS = LEFT
-% +Z-AXIS = UP
-wheelTransform = [ 1 0 wheelBase;    % Left Wheel (_LeftWheel)
-                  -1 0 wheelBase ];  % Right Wheel (_RightWheel)
+% Setup a matrix to map XYZ chassis velocities to wheel velocities
+wheelMatrix = [ 1 0 wheelBase;    % Left Wheel (_LeftWheel)
+               -1 0 wheelBase ];  % Right Wheel (_RightWheel)
 
-
-params.wheelVelocityMatrix = wheelTransform ./ wheelRadius;
-params.wheelEffortMatrix = wheelTransform * wheelRadius;      
-
+params.wheelVelocityMatrix = wheelMatrix ./ wheelRadius;
+params.wheelEffortMatrix = wheelMatrix * wheelRadius;      
 
 % Trajectory generator
 trajGen = HebiTrajectoryGenerator();
--- kits/rosie/setupOmniBase.m
+++ kits/rosie/setupOmniBase.m
@@ -1,11 +1,12 @@
 function [ params, trajGen ] = setupOmniBase()
-%SETUPEDWARD creates various models for the Edward kit
-
-% Initialize structs
-params = struct(); % various constant parameters
-%% %%%%%%%%%%%%%%%%%%%%%
-% Omni Base Kinematics %
-%%%%%%%%%%%%%%%%%%%%%%%%
+%SETUPOMNIBASE initializes the kinematic and control parameters for a
+%3-wheeled omni-drive base.
+%
+% FRAME CONVENTION:
+% ORIGIN = MID-POINT OF THE CHASSIS WHERE ALL 3 WHEEL AXES INTERSECT
+% +X-AXIS = FORWARD
+% +Y-AXIS = LEFT
+% +Z-AXIS = UP
 
 wheelRadius = .150 / 2;  % m
 wheelBase = .235;   % m (center of omni to origin of base)
@@ -18,12 +19,16 @@ params.maxRotSpeed = params.maxLinSpeed / wheelBase; % rad/s
 params.chassisCoM = [0; 0; wheelRadius + 0.005];  % m
 params.chassisMass = 12;  % kg
 
+params.wheelNames = { '_Wheel1', ...
+                      '_Wheel2', ...
+                      '_Wheel3' };
 params.numWheels = 3;
 
-% Setup angles with each wheel
-a1 = pi/6;          % rad (30 deg)
-a2 = a1 + 2*pi/3;   % rad (150 deg)
-a3 = a2 + 2*pi/3;   % rad (270 deg)
+% Setup the transforms from the origin of the chassis to each wheel,
+% starting with the angles that the output axes point out at
+a1 = deg2rad(-60);       % Front Right (_Wheel1)
+a2 = a1 + deg2rad(120);  % Front Left (_Wheel2)
+a3 = a2 + deg2rad(120);  % Rear (_Wheel3)
 
 wheelBaseFrames(:,:,1) = eye(4);
 wheelBaseFrames(:,:,2) = eye(4);
@@ -33,21 +38,16 @@ wheelBaseFrames(1:3,4,1) = [wheelBase*cos(a1); wheelBase*sin(a1); wheelRadius];
 wheelBaseFrames(1:3,4,2) = [wheelBase*cos(a2); wheelBase*sin(a2); wheelRadius];
 wheelBaseFrames(1:3,4,3) = [0; -1*wheelBase; wheelRadius];
 
-wheelBaseFrames(1:3,1:3,1) = R_z(a1)*R_y(pi/2);
-wheelBaseFrames(1:3,1:3,2) = R_z(a2)*R_y(pi/2);
-wheelBaseFrames(1:3,1:3,3) = R_z(a3)*R_y(pi/2);
+wheelBaseFrames(1:3,1:3,1) = R_z(a1) * R_y(pi/2);
+wheelBaseFrames(1:3,1:3,2) = R_z(a2) * R_y(pi/2);
+wheelBaseFrames(1:3,1:3,3) = R_z(a3) * R_y(pi/2);
 
 params.wheelBaseFrames = wheelBaseFrames;
 
 % Maps XYZ chassis velocities to wheel velocities
-%
-% FRAME CONVENTION (FROM THE "DRIVER'S SEAT"):
-% +X-AXIS = FORWARD
-% +Y-AXIS = LEFT
-% +Z-AXIS = UP
-wheelTransform = [ -0.866 -0.500  wheelBase;   % Front Right (_Wheel1)
-                    0.866 -0.500  wheelBase;   % Front Left (_Wheel2)
-                    0.000  1.000  wheelBase ]; % Rear Wheel (_Wheel3)
+wheelTransform = [ sin(a1) -cos(a1)  wheelBase;   % Front Right (_Wheel1)
+                   sin(a2) -cos(a2)  wheelBase;   % Front Left (_Wheel2)
+                   sin(a3) -cos(a3)  wheelBase ]; % Rear (_Wheel3)
 
 params.wheelVelocityMatrix = wheelTransform ./ wheelRadius;
 params.wheelEffortMatrix = wheelTransform * wheelRadius;                          
