4cd1775ed2113f6c84fe0928355998ed5bb40344
Dave Rollinson <daverolli@gmail.com>
2018-07-20 10:53:20 -0400

    Updated startup.m to include subfolders

--- kits/rosie/startup.m
+++ kits/rosie/startup.m
@@ -1,8 +1,16 @@
 function [] = startup()
-% startup sets up libraries and should be run once on startup.
+% startup.m sets up libraries and paths and should be run once on startup.
+% 
+% HEBI Robotics
+% Jun 2018
 
 localDir = fileparts(mfilename('fullpath'));
+
+% Run the include script from the top level of the examples
 includeScript = fullfile(localDir, '..', '..', 'include', 'include.m');
 run(includeScript);
+
+% Add this folder and all its subfolders
+addpath(genpath(localDir));
 
 end
