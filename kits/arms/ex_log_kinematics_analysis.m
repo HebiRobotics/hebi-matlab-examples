7e85a850ba9fc5891874bf4736bb9bcd80438f2f
Dave Rollinson <dave@hebirobotics.com>
2018-10-30 11:35:05 -0400

    Update and rename ex_kinematics_analysis.m to ex_log_kinematics_analysis.m

--- kits/arms/ex_log_kinematics_analysis.m
+++ kits/arms/ex_log_kinematics_analysis.m
@@ -8,6 +8,6 @@
 % from your own arm.  You want to make sure that the HRDF file matches the
 % arm that used in the logged data :-).
 hebilog = HebiUtils.loadGroupLog('logs/3-DoF_arm_example_log');
-kin = HebiKinematics('hrdf/3-DoF_arm_example');
+kin = HebiKinematics('hrdf/3-DoF_arm');
 
-arm_kinematics_analysis( hebilog, kin ); 
+kinematics_analysis( hebilog, kin ); 
