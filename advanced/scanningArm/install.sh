66b4b92daed435d974cd543da775fab7f25d8689
Dave Rollinson <dave@hebirobotics.com>
2018-11-02 12:25:11 -0400

    Fixed typo in install.sh

--- advanced/scanningArm/install.sh
+++ advanced/scanningArm/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Run this script to create a dynamic link to the rosieStart script.
+#Run this script to create a dynamic link to the robotStart script.
 #Once this has executed, you can run the program anywhere by typing "robotStart"
 ROBOT_FILE="/robotStart.sh"
 sudo ln -s $(dirname $(readlink -f $0))$ROBOT_FILE /usr/bin/robotStart
