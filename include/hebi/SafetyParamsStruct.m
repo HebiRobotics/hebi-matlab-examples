32c26eabf564c7811d7ffa5784f01c34d661aede
Dave Rollinson <daverolli@gmail.com>
2019-04-18 15:41:46 -0400

    Updated to API 1.6.1

--- include/hebi/SafetyParamsStruct.m
+++ include/hebi/SafetyParamsStruct.m
@@ -4,11 +4,29 @@ function struct = SafetyParamsStruct()
 %   The struct created by this function can be used to set a variety of
 %   safety limits and strategy parameters on a group of modules.
 %
+%   The online documentation provides more information about the 
+%   different control parameters:
+%   http://docs.hebi.us/core_concepts.html#safety_controller
+%
+%   SafetyParamStructs can be loaded and saved to an XML file format with 
+%   functions provided in HebiUtils.  This format is the preferred way
+%   of storing and loading parameters for different demos / applications.
+%   More information on the XML format can be found at:
+%   https://github.com/HebiRobotics/hebi-xml/blob/master/SAFETY.md
+%
 %   Empty entries in the struct and NaNs for any individual values in
 %   safety parameters are ignored, i.e., any existing setting on the
 %   device for that parameter will remain unmodified.
 %
-%   Example (manually setting safety limits):
+%   Example (Loading safety parameters from XML file and setting on a group):
+%       safetyParams = HebiUtils.loadSafetyParams('mySafetyParams.xml');
+%       group.send('SafetyParams', safetyParams);
+%
+%   Example (Saving safety parameters to XML file)
+%       safetyParams = group.getSafetyParams();
+%       HebiUtils.saveSafetyParams(safetyParams, 'mySafetyParams.xml');
+%
+%   Example (Manually setting safety limits):
 %       limits = SafetyParamsStruct()
 %       limits.positionMinLimit = [-pi -pi];
 %       limits.positionMaxLimit = [+pi +pi];
@@ -18,19 +36,19 @@ function struct = SafetyParamsStruct()
 %   enumerated values with the meaning below:
 %
 %   'mStopStrategy'
-%       nan = Unknown
+%       NaN = Unknown
 %        0  = Disabled
 %        1  = Motor Off
 %        2  = Motor Hold
 %
 %   'positionLimitStrategy'
-%       nan = Unknown
+%       NaN = Unknown
 %        0  = Disabled
 %        1  = Motor Off
 %        2  = Hold Position
 %        3  = Damped Spring
 %
-%   See also HebiGroup, HebiUtils.send
+%   See also HebiGroup, HebiUtils.saveSafetyParams, HebiUtils.loadSafetyParams
 
 %   Copyright 2014-2019 HEBI Robotics, Inc.
 struct = javaObject(hebi_load('SafetyParamsStruct'));
