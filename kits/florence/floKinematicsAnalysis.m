899744ccca1cba0e066366420be834c85b6c0afa
Dave Rollinson <dave@hebirobotics.com>
2019-04-26 18:44:57 +0900

    Add files via upload

---
+++ kits/florence/floKinematicsAnalysis.m
@@ -0,0 +1,105 @@
+function [] = floKinematicsAnalysis( log, kin, legIndex )
+%%
+% Use a log file and kinematic info for an arm and plot end-effector
+% motion and tracking error.
+%
+% Dave Rollinson
+% Jul 2018
+
+% Convert log file to struct to speed up reading.
+log = struct(log);
+    
+logLength = length(log.time);
+cmdXYZ = nan(logLength,3);
+fbkXYZ = nan(logLength,3); 
+cmdVel_6DoF = nan(logLength,6);
+fbkVel_6DoF = nan(logLength,6); 
+
+%%
+% XYZ position tracking
+leg = 2;
+
+% Calculate kinematics for commanded and feedback
+for i = 1:length(log.time)
+    tempFrame = kin.leg{leg}.getFK('endeffector',log.positionCmd(i,legIndex{leg}));
+    cmdXYZ(i,:) = tempFrame(1:3,4);
+    tempFrame = kin.leg{leg}.getFK('endeffector',log.position(i,legIndex{leg}));
+    fbkXYZ(i,:) = tempFrame(1:3,4);
+end
+
+% Calculate XYZ position Error
+errorXYZ = fbkXYZ - cmdXYZ;
+errorNorm = sqrt( errorXYZ(:,1).^2 + ...
+                  errorXYZ(:,2).^2 + ...
+                  errorXYZ(:,3).^2 );
+                 
+%%
+% XYZ velocity tracking
+
+% Calculate Jacobians for commanded and feedback
+for i = 1:length(log.time)
+    J_cmd(:,:,i) = kin.leg{leg}.getJacobian( 'endeffector', log.positionCmd(i,legIndex{leg}) );
+    J_fbk(:,:,i) = kin.leg{leg}.getJacobian( 'endeffector', log.position(i,legIndex{leg}) );
+    
+    cmdVel_6DoF(i,:) = J_fbk(:,:,i) * log.velocityCmd(i,legIndex{leg})';
+    fbkVel_6DoF(i,:) = J_fbk(:,:,i) * log.velocity(i,legIndex{leg})';
+end
+
+% Calculate XYZ velocity Error
+errorVel_6DoF = fbkVel_6DoF - cmdVel_6DoF;
+errorVelNorm = sqrt( errorVel_6DoF(:,1).^2 + ...
+                     errorVel_6DoF(:,2).^2 + ...
+                     errorVel_6DoF(:,3).^2 );
+                 
+                 
+   
+%%
+% Plotting
+
+fbkXYZ_mm = 1000 * fbkXYZ;
+cmdXYZ_mm = 1000 * cmdXYZ;
+errorXYZ_mm = 1000 * errorXYZ;
+errorNorm_mm = 1000 * errorNorm;
+
+figure(501);
+plot3( fbkXYZ_mm(:,1), fbkXYZ_mm(:,2), fbkXYZ_mm(:,3), 'b' );
+hold on;
+plot3( cmdXYZ_mm(:,1), cmdXYZ_mm(:,2), cmdXYZ_mm(:,3), 'r:' );
+hold off;
+
+title('End-Effector Tracking');
+xlabel('x (mm)');
+ylabel('y (mm)');
+zlabel('z (mm)');
+axis equal;
+grid on;
+legend feedback command;
+
+% If there were any commanded positions, plot the tracking error
+if any(~isnan(cmdXYZ(:)))
+    figure(502);
+    subplot(2,1,1);
+    plot(log.time, errorXYZ_mm(:,1), 'r');
+    hold on;
+    plot(log.time, errorXYZ_mm(:,2), 'g');
+    plot(log.time, errorXYZ_mm(:,3), 'b');
+    hold off;
+
+    title('End-Effector XYZ Error - Time');
+    xlabel('time (sec)');
+    ylabel('error (mm)');
+    grid on;
+    xlim([0 log.time(end)]);
+    legend x y z;
+
+    subplot(2,1,2);
+    plot(log.time, errorNorm_mm, 'k');
+    title('End-Effector Total Error - Time');
+    xlabel('time (sec)');
+    ylabel('error (mm)');
+    grid on;
+    xlim([0 log.time(end)]);        
+end
+
+
+
