9147e7196a12a47bf95595e376093416895ec42f
Dave Rollinson <dave@hebirobotics.com>
2024-08-23 14:12:11 -0400

    Leader-follower w/ force feedback.

---
+++ kits/arms/ex_leader_follower.m
@@ -0,0 +1,89 @@
+% Arm Gravity Compensation Demo
+%
+% Features:      Demo where the arm can be interacted with and moved around
+%                while in a zero-force gravity-compensated mode.
+%
+% Requirements:  MATLAB 2013b or higher
+%
+% Author:        Dave Rollinson
+%                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+% Date:          Oct 2018
+
+% Copyright 2017-2018 HEBI Robotics
+
+%% Setup
+clear *;
+close all;
+
+HebiLookup.initialize();
+pause(1);
+
+% Instantiate the arm kit based on the config files in config/${name}.yaml
+% If your kit has a gas spring, you need to uncomment the offset lines
+% in the corresponding config file.
+[ arm, params ] = setupArm( 'leader' );
+[ arm2, params2 ] = setupArm( 'follower' );
+
+arm2.trajGen.setMinDuration(0.2);
+
+
+localDir = params.localDir;
+enableLogging = true;
+
+% Start background logging 
+if enableLogging
+   logFile = arm.group.startLog('dir',[localDir '/logs']); 
+end
+
+%% Gravity compensated mode
+disp('Commanded gravity-compensated zero torques to the arm.');
+disp('Press ESC to stop.');
+
+% Keyboard input
+kb = HebiKeyboard();
+keys = read(kb);
+while ~keys.ESC   
+    keys = read(kb);
+    
+    arm.update();
+    arm2.update();
+    
+    arm2.setGoal( arm.state.fbk.position );
+    
+    effortError = arm2.state.fbk.effort - arm2.state.fbk.effortCmd;
+    
+    % Tweak to reduce the applied torque errors when they're small.  This
+    % helps make the arm feel better when there are only small errors due
+    % to modeling / spring error, but still apply torques when there's
+    % significant error from collisions / disturbances
+    effortScales = 0.5*[16 16 16 9 9 3];  % Scales depending on joint
+    fudgeFactor = abs(effortError) ./ effortScales;
+    fudgeFactor = min( fudgeFactor, 1.0 ); % Cap at 1 (don't amplify errors)
+    
+    modifiedError = fudgeFactor .* effortError;
+    
+    arm.state.cmdEffort = arm.state.cmdEffort - modifiedError;
+    
+    arm.send();
+    arm2.send();
+end
+
+%%
+if enableLogging
+    
+   hebilog = arm.group.stopLogFull();
+   
+   % Plot tracking / error from the joints in the arm.  Note that there
+   % will not by any 'error' in tracking for position and velocity, since
+   % this example only commands effort.
+   HebiUtils.plotLogs(hebilog, 'position');
+   HebiUtils.plotLogs(hebilog, 'velocity');
+   HebiUtils.plotLogs(hebilog, 'effort');
+   
+   % Plot the end-effectory trajectory and error
+   kinematics_analysis( hebilog, arm.kin );
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Feel free to put more plotting code here %
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
