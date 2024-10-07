cb28680178009db7005ad5a69fa8c1a7917dc9b4
Florian Enner <florian@hebirobotics.com>
2024-10-07 16:16:57 +0200

    refactored gripper and mobileIO creation to match python

--- kits/arms/createMobileIOFromConfig.m
+++ kits/arms/createMobileIOFromConfig.m
@@ -1,21 +1,23 @@
 function mobileIO = createMobileIOFromConfig( config )
+    userData = config.userData;
 
     % Required keys for the mobile IO configuration
     requiredKeys = {'mobile_io_family', 'mobile_io_name', 'mobile_io_layout'};
 
-    % Validate the mobile_io configuration
-    if all(isfield(config.userData, requiredKeys))
-        
-        % Check that all required fields are present and are non-empty strings
-        if all(cellfun(@(key) ischar(config.userData.(key)), requiredKeys))
-            
-            mobileIO = HebiMobileIO.findDevice(config.userData.mobile_io_family, config.userData.mobile_io_name);
-            mobileIO.group.send('MobileLayout', fullfile('config', config.userData.mobile_io_layout));
-
-        else
-            error('HEBI config "user_data"''s fields %s must contain strings, not parseable as strings', strjoin(requiredKeys, ', '));
-        end
-    else
+    % Validate the user data configuration
+    if ~all(isfield(userData, requiredKeys))
         error('HEBI config "user_data" field must contain the keys: %s', strjoin(requiredKeys, ', '));
     end
+    if ~all(cellfun(@(key) ischar(userData.(key)), requiredKeys))
+        error('HEBI config "user_data" fields %s must contain strings', strjoin(requiredKeys, ', '));
+    end
+
+    % Load layout relative to the config file
+    [cfgDir, ~, ~] = fileparts(config.configLocation);
+    layoutFile = fullfile(cfgDir, userData.mobile_io_layout);
+
+    % Validate the mobile_io configuration
+    mobileIO = HebiMobileIO.findDevice(userData.mobile_io_family, userData.mobile_io_name);
+    mobileIO.group.send('MobileLayout', layoutFile); % TODO: replace with a HebiUtils command
+
 end
